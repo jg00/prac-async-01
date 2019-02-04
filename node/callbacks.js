c218525498b3dd448b6b78675c6de6a5afdb2717
Jack Gamboa <jgamboa929@gmail.com>
2019-02-04 09:48:54 -0600

    callback.js - handling errors

--- node/callbacks.js
+++ node/callbacks.js
@@ -2,7 +2,7 @@
 // is a callback that’s executed in the next iteration of the event loop.
 // or process.nextTick()
 
-// Callbacks are not async by default.
+// Callbacks are not necessarily async by default.
 
 function doAsyncTask(cb) {
   setImmediate(() => {
@@ -16,7 +16,21 @@ function doAsyncTask(cb) {
   });
 }
 
-doAsyncTask(_ => console.log(message));
+// doAsyncTask(_ => console.log(message));
 
 // message defined here intentionally
 let message = "Callback Called";
+
+// 2 Handling callback errors and passing the error up the chain
+const fs = require("fs");
+
+function readFileThenDo(next) {
+  fs.readFile("./blah.nofile", { encoding: "utf8" }, (err, data) => {
+    next(err, data);
+  });
+}
+
+readFileThenDo((err, data) => {
+  if (err) console.error(err);
+  else console.log(data);
+});
